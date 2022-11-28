cd3325cec68ac8210c9e445937a6797f2b3f9f69
Marco Chatillon <marcochatillon13@gmail.com>
2022-11-28 20:43:25 +0000

    add header

---
+++ includes/push_swap.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdoroana <mdoroana@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 17:01:54 by mdoroana          #+#    #+#             */
+/*   Updated: 2022/11/28 19:30:40 by mdoroana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <../libft/libft.h>
+# include <../ft_printf/ft_printf.h>
+# include <limits.h>
+
+typedef struct s_sort
+{
+	
+}				t_sort;
+
+#endif
