ea2e2d8b02fba36fae3a81b892977c1674959638
Marco Chatillon <marcochatillon13@gmail.com>
2022-12-02 20:25:11 +0000

    added utils to support for radix sort

---
+++ srcs/utils.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdoroana <mdoroana@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/01 20:47:19 by mdoroana          #+#    #+#             */
+/*   Updated: 2022/12/01 20:57:12 by mdoroana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/push_swap.h"
+
+int	is_sorted(t_list *a)
+{
+	while (a->next)
+	{
+		if (a->data > a->next->data)
+			return (0);
+		a = a->next;
+	}
+	return (1);
+}
+
+void	is_min_max(int	*min, int *max, t_list *a)
+{
+	while (a)
+	{
+		if (a->data < *min)
+			*min = a->data;
+		if (a->data > *max)
+			*max = a->data;
+		a = a->next;
+	}
+}
