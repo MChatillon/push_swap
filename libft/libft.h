6634a92f0836a4182f7846b1a622239664320294
Marco Chatillon <marcochatillon13@gmail.com>
2022-12-02 19:56:01 +0000

    remove void content, add int data

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mdoroana <mdoroana@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/16 16:18:32 by mdoroana          #+#    #+#             */
-/*   Updated: 2022/03/02 17:05:52 by mdoroana         ###   ########.fr       */
+/*   Updated: 2022/12/01 19:28:05 by mdoroana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,10 @@
 
 typedef struct s_list
 {
-	void			*content;
+	int				index;
+	int				data;
 	struct s_list	*next;
-}				t_list;
+}t_list;
 
 int				ft_atoi(const char *str);
 
@@ -91,7 +92,7 @@ int				ft_toupper(int c);
 
 void			ft_striteri(char *s, void (*f)(unsigned int, char*));
 
-t_list			*ft_lstnew(void *content);
+t_list			*ft_lstnew(int data);
 
 int				ft_lstsize(t_list *lst);
 
@@ -101,12 +102,12 @@ void			ft_lstadd_front(t_list **lst, t_list *new);
 
 void			ft_lstadd_back(t_list **lst, t_list *new);
 
-void			ft_lstdelone(t_list *lst, void (*del)(void*));
+void			ft_lstdelone(t_list *lst, void (*del)(int));
 
-void			ft_lstclear(t_list **lst, void (*del)(void*));
+void			ft_lstclear(t_list **lst, void (*del)(int));
 
-void			ft_lstiter(t_list *lst, void (*f)(void*));
+void			ft_lstiter(t_list *lst, void (*f)(int));
 
-t_list			*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *));
+t_list			*ft_lstmap(t_list *lst, int (*f)(int), void (*del)(int));
 
 #endif
